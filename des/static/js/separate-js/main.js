2dbbb0a5c904387380252b1eefcc1cc935025f78
Marina Tiskovets <m.tyskovets@gmail.com>
2024-08-08 15:41:07 +0300

    modals

--- des/static/js/separate-js/main.js
+++ des/static/js/separate-js/main.js
@@ -1,19 +1,6 @@
 $(document).ready(function () {
 
-    $(document).on('focus', '.js-head-search-toggler', function () {
-        $(this).parents('.js-head-search').addClass('is-extended');
-    });
-
-    $(document).on('show.bs.modal', '.js-modal-locker', function () {
-        locker.lock();
-    });
-    $(document).on('hidden.bs.modal', '.js-modal-locker', function () {
-        locker.unlock();
-    });
-
     toggle.initialize();
-
-    drawer.initialize();
 
     // Example autocomplete
     var jobs = [
@@ -22,27 +9,40 @@ $(document).ready(function () {
         {value: 'Дизайнер', data: 'designer'}
     ];
 
-    var fieldIds = ['#query', '#location'];
-
-    for (var i= 0; i < fieldIds.length; i++) {
-
-        var element = $(fieldIds[i]);
-
-        element.autocomplete({
-            lookup: jobs,
-            width: 'flex',
-            appendTo: element.parent(),
-            onSelect: function (suggestion) {
-                console.log('You selected: ' + suggestion.value + ', ' + suggestion.data);
-            }
-        });
-    }
+    $('#autocomplete').autocomplete({
+        lookup: jobs,
+        onSelect: function (suggestion) {
+            console.log('You selected: ' + suggestion.value + ', ' + suggestion.data);
+        }
+    });
 
     $('.js-form-focus').toggleFocusInput();
 
-    $('.js-once-touch-field').on('focus', function () {
-        $(this).parents('.js-once-touch-form').addClass('is-touched');
+    $(document).on('show.bs.modal', '.js-modal-locker', function () {
+        blockScroll.block();
     });
+    $(document).on('hidden.bs.modal', '.js-modal-locker', function () {
+        blockScroll.unblock();
+    });
+
+    $('.js-slider').owlCarousel({
+        margin: 5,
+        loop: false,
+        autoWidth: true,
+        items: 5,
+        dots: false,
+        mouseDrag: false,
+        nav: true,
+    })
+
+    // For dropdowns (Clicks inside do not close the dropdown)
+    $(document).on(
+        'click.bs.dropdown.data-api',
+        '.js-keep-inside-clicks-open',
+        function (e) {
+            e.stopPropagation();
+        }
+    );
 
     $('.js-field-dropdown').on('focus', function () {
         $(this).parents('.js-field-dropdown-container').addClass('is-open');
@@ -60,38 +60,6 @@ $(document).ready(function () {
         });
     });
 
-    (function ($) {
-        var $collapse = $('.js-toggle-collapse-parent');
-
-        $collapse.on('show.bs.collapse', function () {
-            $(this).parent().addClass('is-open');
-        });
-        $collapse.on('hide.bs.collapse', function () {
-            $(this).parent().removeClass('is-open');
-        });
-    })(jQuery);
-
-    // For dropdowns (Clicks inside do not close the dropdown)
-    $(document).on(
-        'click.bs.dropdown.data-api',
-        '.js-keep-inside-clicks-open',
-        function (e) {
-            e.stopPropagation();
-        }
-    );
-
-    $('.js-scroll-tab-into-view').on('shown.bs.tab', function () {
-        var element = $($(this).data('target'))[0];
-        element.scrollIntoView({behavior: "smooth"});
-    });
-
-    $('.js-tab-has-tooltip').on('shown.bs.tab', function () {
-        var element = $($(this).data('target'))[0];
-        var id = $(element).attr('id');
-        var tab = $("[data-target='#" + id + "']").parent();
-        tab.find('.js-tab-tooltip').addClass('u-hidden');
-    });
-
     $('.js-toggle-btn').on('click', function () {
         var container = $('.js-toggle-container');
 
@@ -101,125 +69,37 @@ $(document).ready(function () {
             container.addClass('is-open');
         }
     });
-
-    $('.js-input-show-modal').on('change', function() {
-        if ($(this).is(':not(:checked')) {
-            $($(this).attr('data-target')).modal('show');
-        }
-    });
-
-    $('.js-change-input-group').on('change', function() {
-        var container = $(this).parents('.js-input-group');
-        var inputs = container.find('input[type=checkbox]');
-        inputs.prop('checked', $(this).prop('checked'));
-
-        if (container.find('input[type=checkbox]:checked.js-change-input').length === 0) {
-            container.find('.js-change-input').prop({
-                disabled: true
-            });
-        } else {
-            container.find('.js-change-input').prop({
-                disabled: false
-            });
-        }
-    });
-
-    $('.js-change-input').on('change', function() {
-        var checked = $(this).prop('checked');
-        var container = $(this).parents('.js-input-group');
-
-        if ($(this).is(':checked')) {
-            container.find('.js-change-input-group').prop({
-                indeterminate: true,
-                checked: container.find('.js-change-input-group').prop('checked', false)
-            });
-        } else {
-            if (container.find('input[type=checkbox]:checked.js-change-input').length === 0) {
-                container.find('input[type=checkbox]').prop({
-                    indeterminate: false,
-                    checked: checked
-                });
-                container.find('.js-change-input').prop({
-                    disabled: true
-                });
-            }
-        }
-    });
-
-    $('.js-carousel').owlCarousel({
-        margin: 30,
-        nav:true,
-        loop: false,
-        autoWidth: true,
-        items: 1,
-        dots: false,
-        mouseDrag: false,
-    });
-
-    $('.js-click-anchor').on('click', function() {
-        var input = $($(this).data('anchor'));
-
-        if (input.length > 0) {
-            $('html, body').animate({ scrollTop: 0 }, 'slow');
-            input.focus();
-        }
-    });
 });
 
-var fixSection = (function ($) {
-    var defaults = {
-        fixedSelector: '.js-fix-section',
-        placeholderBlockSelector: '.js-height-placeholder',
-        fixedClass: 'is-top-fixed',
-        scrolledClass: 'is-scrolled',
-        endingScrollClass: 'is-ending-scroll'
+var blockScroll = (function ($) {
+    var noScrollClass = 'no-scroll',
+        isBlocked = false;
+
+    var block = function () {
+        if (isBlocked) {
+            return;
+        }
+
+        isBlocked = true;
+        // if (yii.version.isDesktop) {
+        //     $('body').css('paddingRight', window.innerWidth - document.documentElement.clientWidth);
+        // }
+        $('html').addClass(noScrollClass);
     };
 
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {}),
-            offsetTop = $(settings.fixedSelector).offset().top,
-            placeholderBlock = $(settings.fixedSelector).find(settings.placeholderBlockSelector),
-            scrolled = false;
-
-        $(window).on('scroll', function () {
-            var scrollTop = $(window).scrollTop();
-            var height = $(settings.fixedSelector).height() + offsetTop;
-
-            if (offsetTop < scrollTop) {
-                $(settings.fixedSelector).addClass(settings.fixedClass);
-            } else {
-                $(settings.fixedSelector).removeClass(settings.fixedClass);
-            }
-
-            if (scrollTop > height) {
-                $(settings.fixedSelector)
-                    .addClass(settings.scrolledClass)
-                    .removeClass(settings.endingScrollClass);
-
-                placeholderBlock.css('minHeight', placeholderBlock.children().outerHeight());
-
-                scrolled = true;
-            } else {
-                $(settings.fixedSelector).removeClass(settings.scrolledClass);
-
-                if ((height - scrollTop) <= 50) {
-                    if (scrolled) $(settings.fixedSelector).addClass(settings.endingScrollClass);
-                } else {
-                    $(settings.fixedSelector).removeClass(settings.endingScrollClass);
-
-                    placeholderBlock.css('minHeight', '');
-                }
-
-                scrolled = false;
-            }
-        }).trigger('scroll');
+    var unblock = function () {
+        $('html').removeClass(noScrollClass);
+        // if (yii.version.isDesktop) {
+        //     $('body').css('paddingRight', 0);
+        // }
+        isBlocked = false;
     };
 
     return {
-        initialize: initialize
+        block: block,
+        unblock: unblock
     };
 })(jQuery);
-
 
 var toggle = (function ($) {
     var defaults = {
@@ -245,155 +125,6 @@ var toggle = (function ($) {
 
             return false;
         });
-    };
-
-    return {
-        initialize: initialize
-    };
-})(jQuery);
-
-var drawer = (function ($) {
-    var settings = {
-        drawer: '.js-drawer',
-        toggler: '.js-drawer-toggler',
-        stateClass: 'is-open',
-    };
-
-    var initialize = function () {
-        var toggler = $(settings.toggler),
-            drawer = $(settings.drawer),
-            stateClass = settings.stateClass;
-
-        toggler.on('click', function () {
-            if (drawer.hasClass(stateClass)) {
-                drawer.removeClass(stateClass);
-
-                locker.unlock();
-            } else {
-                drawer.addClass(stateClass);
-
-                locker.lock();
-            }
-        });
-    };
-
-    return {
-        initialize: initialize,
-    };
-})(jQuery);
-
-
-var locker = (function ($) {
-    var settings = {
-        element: 'html',
-        lockedClass: 'no-scroll'
-    };
-
-    var lock = function () {
-        var padding = window.innerWidth - document.documentElement.clientWidth;
-
-        $(settings.element)
-            .addClass(settings.lockedClass)
-            .css('paddingRight', padding);
-    };
-
-    var unlock = function () {
-        $(settings.element)
-            .removeClass(settings.lockedClass)
-            .css('paddingRight', 0);
-    };
-
-    return {
-        lock: lock,
-        unlock: unlock
-    };
-})(jQuery);
-
-
-var scrollToTop = (function ($) {
-    var defaults = {
-        speed: 500,
-        classList: 'gui-btn-up material-icons',
-    };
-
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {});
-        var height = $(window).height();
-
-        var $button = $('<button/>', {
-            type: 'button',
-            class: settings.classList
-        });
-
-        $button.on('click', function () {
-            $('body, html').animate({
-                scrollTop: 0
-            }, settings.speed);
-        });
-
-        var $appended = $button.appendTo('body').hide();
-
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > height) {
-                $appended.fadeIn();
-            } else {
-                $appended.fadeOut();
-            }
-        });
-    };
-
-    return {
-        initialize: initialize
-    };
-})(jQuery);
-
-
-var floatingHeader = (function ($) {
-    var defaults = {
-        topCssClass: 'sticky-top',
-        topExtendCssClass: 'sticky-top-extend'
-    };
-
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {});
-
-        var $header = $('#' + settings.id);
-        var $parent = $header.parent();
-        var $headerTop = $header.find(':first-child');
-        var $extender = $('[data-extend="floating-header"][data-target="#' + settings.id + '"]');
-
-        $(window).on('scroll', function () {
-            var fullHeight = 0;
-            var headerTopHeight = $headerTop.outerHeight(true);
-
-            $header.children().filter(':visible').each(function() {
-                fullHeight += $(this).outerHeight(true);
-            });
-
-            $parent.css('height', fullHeight);
-
-            if ($(window).scrollTop() > $parent.offset().top + headerTopHeight) {
-                $header
-                    .addClass(settings.topCssClass)
-                    .removeClass(settings.topExtendCssClass)
-                    .css({top: - headerTopHeight});
-            } else {
-                $header
-                    .removeClass(settings.topCssClass)
-                    .removeClass(settings.topExtendCssClass)
-                    .css({top: 'initial'});
-            }
-        });
-
-        $extender.on('click', function () {
-            if ($header.hasClass(settings.topCssClass)) {
-                $header
-                    .addClass(settings.topExtendCssClass)
-                    .css({top: 0});
-            }
-        });
-
-        $(window).trigger('scroll');
     };
 
     return {
--- mob/static/js/separate-js/main.js
+++ mob/static/js/separate-js/main.js
@@ -1,14 +1,6 @@
 $(document).ready(function () {
-    $(document).on('show.bs.modal', '.js-modal-locker', function (event) {
-        locker.lock();
-    });
-    $(document).on('hidden.bs.modal', '.js-modal-locker', function (event) {
-        locker.unlock();
-    });
 
     toggle.initialize();
-
-    drawer.initialize();
 
     // Example autocomplete
     var jobs = [
@@ -17,6 +9,8 @@ $(document).ready(function () {
         {value: 'Дизайнер', data: 'designer'}
     ];
 
+    $('.js-form-focus').toggleFocusInput();
+
     $('#autocomplete').autocomplete({
         lookup: jobs,
         onSelect: function (suggestion) {
@@ -24,28 +18,12 @@ $(document).ready(function () {
         }
     });
 
-    $('.js-form-focus').toggleFocusInput();
-
-    $('.js-once-touch-field').on('focus', function () {
-        $(this).parents('.js-once-touch-form').addClass('is-active');
+    $(document).on('show.bs.modal', '.js-modal-locker', function () {
+        blockScroll.block();
     });
-
-    (function ($) {
-        var $collapse = $('.js-toggle-collapse-parent');
-
-        $collapse.on('show.bs.collapse', function () {
-            $(this).parent().addClass('is-open');
-        });
-        $collapse.on('hide.bs.collapse', function () {
-            $(this).parent().removeClass('is-open');
-        });
-    })(jQuery);
-
-    collapseContentHeight.initialize();
-
-    if (document.getElementById('header')) {
-        floatingHeader.initialize({id: 'header'});
-    }
+    $(document).on('hidden.bs.modal', '.js-modal-locker', function () {
+        blockScroll.unblock();
+    });
 
     // For dropdowns (Clicks inside do not close the dropdown)
     $(document).on(
@@ -56,18 +34,6 @@ $(document).ready(function () {
         }
     );
 
-    $('.js-scroll-tab-into-view').on('shown.bs.tab', function () {
-        var element = $($(this).data('target'))[0];
-        element.scrollIntoView({behavior: "smooth"});
-    });
-
-    $('.js-tab-has-tooltip').on('shown.bs.tab', function () {
-        var element = $($(this).data('target'))[0];
-        var id = $(element).attr('id');
-        var tab = $("[data-target='#" + id + "']").parent();
-        tab.find('.js-tab-tooltip').addClass('u-hidden');
-    });
-
     $('.js-toggle-btn').on('click', function () {
         var container = $('.js-toggle-container');
 
@@ -77,108 +43,37 @@ $(document).ready(function () {
             container.addClass('is-open');
         }
     });
-
-    $('.js-input-show-modal').on('change', function() {
-        if ($(this).is(':not(:checked')) {
-            $($(this).attr('data-target')).modal('show');
-        }
-    });
-
-    $('.js-change-input-group').on('change', function() {
-        var container = $(this).parents('.js-input-group');
-        var inputs = container.find('input[type=checkbox]');
-        inputs.prop('checked', $(this).prop('checked'));
-
-        if (container.find('input[type=checkbox]:checked.js-change-input').length === 0) {
-            container.find('.js-change-input').prop({
-                disabled: true
-            });
-        } else {
-            container.find('.js-change-input').prop({
-                disabled: false
-            });
-        }
-    });
-
-    $('.js-change-input').on('change', function() {
-        var checked = $(this).prop('checked');
-        var container = $(this).parents('.js-input-group');
-
-        if ($(this).is(':checked')) {
-            container.find('.js-change-input-group').prop({
-                indeterminate: true,
-                checked: container.find('.js-change-input-group').prop('checked', false)
-            });
-        } else {
-            if (container.find('input[type=checkbox]:checked.js-change-input').length === 0) {
-                container.find('input[type=checkbox]').prop({
-                    indeterminate: false,
-                    checked: checked
-                });
-                container.find('.js-change-input').prop({
-                    disabled: true
-                });
-            }
-        }
-    });
-
-    handleHorizontalScroll.init();
 });
 
+var blockScroll = (function ($) {
+    var noScrollClass = 'no-scroll',
+        isBlocked = false;
 
-var handleHorizontalScroll = (function($) {
-    var defaults = {
-        container: '.js-scroll-container',
-        scroll: '.js-scroll',
-        button: '.js-scroll-control',
-        offset: 10,
-        scrollSpeed: 200,
+    var block = function () {
+        if (isBlocked) {
+            return;
+        }
+
+        isBlocked = true;
+        // if (yii.version.isDesktop) {
+        //     $('body').css('paddingRight', window.innerWidth - document.documentElement.clientWidth);
+        // }
+        $('html').addClass(noScrollClass);
     };
 
-    function init(params) {
-        var settings = $.extend({}, defaults, params || {});
-        var $containers = $(settings.container);
-
-        $containers.each(function () {
-            var $container = $(this);
-            var $scroll = $container.find(settings.scroll);
-            var $button = $container.find(settings.button);
-            var scrollWidth = getScrollWidth();
-
-            checkVisibilityButton();
-
-            $(window).on('resize', function() {
-                scrollWidth = getScrollWidth();
-                checkVisibilityButton();
-            });
-
-            $scroll.on('scroll', function() {
-                checkVisibilityButton();
-            });
-
-            $button.on('click', function () {
-                $scroll.animate({scrollLeft: scrollWidth}, settings.scrollSpeed);
-            });
-
-            function getScrollWidth() {
-                return $scroll[0].scrollWidth;
-            }
-
-            function checkVisibilityButton() {
-                if ($scroll.scrollLeft() + $scroll.outerWidth(true) + settings.offset >= scrollWidth) {
-                    $button.hide();
-                } else {
-                    $button.show();
-                }
-            }
-        })
-    }
+    var unblock = function () {
+        $('html').removeClass(noScrollClass);
+        // if (yii.version.isDesktop) {
+        //     $('body').css('paddingRight', 0);
+        // }
+        isBlocked = false;
+    };
 
     return {
-        init: init
+        block: block,
+        unblock: unblock
     };
 })(jQuery);
-
 
 var toggle = (function ($) {
     var defaults = {
@@ -204,228 +99,6 @@ var toggle = (function ($) {
 
             return false;
         });
-    };
-
-    return {
-        initialize: initialize
-    };
-})(jQuery);
-
-
-var drawer = (function ($) {
-    var settings = {
-        drawer: '.js-drawer',
-        toggler: '.js-drawer-toggler',
-        stateClass: 'is-open',
-    };
-
-    var initialize = function () {
-        var toggler = $(settings.toggler),
-            drawer = $(settings.drawer),
-            stateClass = settings.stateClass;
-
-        toggler.on('click', function () {
-            if (drawer.hasClass(stateClass)) {
-                drawer.removeClass(stateClass);
-
-                locker.unlock();
-            } else {
-                drawer.addClass(stateClass);
-
-                locker.lock();
-            }
-        });
-    };
-
-    return {
-        initialize: initialize,
-    };
-})(jQuery);
-
-
-var locker = (function ($) {
-    var settings = {
-        element: 'html',
-        lockedClass: 'no-scroll'
-    };
-
-    var lock = function () {
-        $(settings.element)
-            .addClass(settings.lockedClass);
-    };
-
-    var unlock = function () {
-        $(settings.element)
-            .removeClass(settings.lockedClass);
-    };
-
-    return {
-        lock: lock,
-        unlock: unlock
-    };
-})(jQuery);
-
-
-var scrollToTop = (function ($) {
-    var defaults = {
-        speed: 500,
-        classList: 'gui-btn-up material-icons',
-    };
-
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {});
-        var height = $(window).height();
-
-        var $button = $('<button/>', {
-            type: 'button',
-            class: settings.classList
-        });
-
-        $button.on('click', function () {
-            $('body, html').animate({
-                scrollTop: 0
-            }, settings.speed);
-        });
-
-        var $appended = $button.appendTo('body').hide();
-
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > height) {
-                $appended.fadeIn();
-            } else {
-                $appended.fadeOut();
-            }
-        });
-    };
-
-    return {
-        initialize: initialize
-    };
-})(jQuery);
-
-
-var collapseContentHeight = (function ($) {
-    var defaults = {
-        container: '.js-collapse-content-height',
-        initializedCssClass: 'initialized',
-        collapsedCssClass: 'collapsed'
-    };
-
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {});
-
-        $(window).on('resize', function () {
-            $(settings.container).each(function () {
-                var $container = $(this);
-                var totalHeight = 0;
-                var maxHeight = $container.data('max-height');
-                var $trigger = $('[data-toggle="collapse-content-height"]', $container);
-
-                $container.children().filter(':visible').each(function() {
-                    totalHeight += $(this).outerHeight(true);
-                });
-
-                if (totalHeight > maxHeight) {
-                    $container.css('maxHeight', maxHeight).addClass(settings.collapsedCssClass);
-                } else {
-                    $container.removeClass(settings.collapsedCssClass);
-                }
-
-                $trigger.on('click', function () {
-                    $container.toggleClass(settings.collapsedCssClass);
-                });
-            });
-        });
-
-        $(window).trigger('resize');
-    };
-
-    return {
-        initialize: initialize
-    };
-})(jQuery);
-
-
-var floatingHeader = (function ($) {
-    var defaults = {
-        topCssClass: 'sticky-top',
-        floatingCssClass: 'floating'
-    };
-
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {});
-        var previousScroll = 0;
-        var $header = $('#' + settings.id);
-        var $child = $header.find('>:first-child');
-
-        $(window).on('scroll', function () {
-            var currentScroll = $(this).scrollTop();
-            var fullHeight = 0;
-
-            $header.children().filter(':visible').each(function () {
-                fullHeight += $(this).outerHeight(true);
-            });
-
-            $header.css('height', fullHeight);
-
-            if ($(window).scrollTop() > $header.offset().top) {
-                $child
-                    .addClass(settings.topCssClass);
-
-                if (currentScroll > previousScroll) {
-                    $child.removeClass(settings.floatingCssClass);
-                } else {
-                    $child.addClass(settings.floatingCssClass);
-                }
-            } else {
-                $child
-                    .removeClass(settings.topCssClass)
-                    .removeClass(settings.floatingCssClass);
-            }
-
-            previousScroll = currentScroll;
-        });
-
-        $(window).trigger('scroll');
-    };
-
-    return {
-        initialize: initialize
-    };
-})(jQuery);
-
-
-var bottomPanel = (function ($) {
-    var defaults = {
-        visibleCssClass: 'is-visible',
-        timeOut: 1000,
-    };
-
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {});
-        var previousScroll = 0;
-        var $panel = $('#' + settings.id);
-        var timer;
-
-        $(window).on('scroll', function () {
-            var currentScroll = $(this).scrollTop();
-
-            if (currentScroll > previousScroll) {
-                $panel.removeClass(settings.visibleCssClass);
-            } else {
-                $panel.addClass(settings.visibleCssClass);
-            }
-
-            if (timer) clearTimeout(timer);
-
-            timer = setTimeout(function () {
-                $panel.addClass(settings.visibleCssClass);
-            }, settings.timeOut);
-
-            previousScroll = currentScroll;
-        });
-
-        $(window).trigger('scroll');
     };
 
     return {
