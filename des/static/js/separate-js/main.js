6f82f702a30f311af20d10306a8a700ce019552c
Marina Tiskovets <m.tyskovets@gmail.com>
2025-02-10 14:32:27 +0200

    test

--- des/static/js/separate-js/main.js
+++ des/static/js/separate-js/main.js
@@ -57,6 +57,7 @@ var locker = (function ($) {
     };
 })(jQuery);
 
+
 var toggle = (function ($) {
     var defaults = {
         itemSelector: '[data-toggle-item]',
@@ -88,50 +89,25 @@ var toggle = (function ($) {
     };
 })(jQuery);
 
-var dropdownManager = (function ($) {
+
+var scrollToTop = (function ($) {
     var defaults = {
-        container: '.js-dropdown-container',
-        opener: '.js-dropdown-container-open',
-        closer: '.js-dropdown-container-close',
-        dropdownParent: '.js-dropdown',
-        dropdownToggle: '[data-bs-toggle="dropdown"]',
-        stateClass: 'is-open'
+        speed: 500,
+        classBtn: 'c-scroll-top-button',
+        classIcon: 'c-ico c-ico--xl c-ico--inherit'
     };
 
     var initialize = function (params) {
         var settings = $.extend({}, defaults, params || {}),
-            container = $(settings.container),
-            stateClass = settings.stateClass;
+            $button = $('<button/>', {type: 'button', class: settings.classBtn})
+                .append('<svg class="' + settings.classIcon + '"><path fill="currentColor" d="M13,20H11V8L5.5,13.5L4.08,12.08L12,4.16L19.92,12.08L18.5,13.5L13,8V20Z"></path></svg>')
+                .on('click', function () {
+                    $('body, html').animate({scrollTop: 0}, settings.speed);
+                })
+                .appendTo('body').hide();
 
-        $(settings.opener).on('click', function (event) {
-            event.stopPropagation();
-
-            if (!container.hasClass(stateClass)) {
-                container.addClass(stateClass);
-            }
-        });
-
-        $(settings.closer).on('click', function (event) {
-            event.stopPropagation();
-
-            if (container.hasClass(stateClass)) {
-                container.removeClass(stateClass);
-
-                var dropdownToggle = $(settings.opener).closest(settings.dropdownParent).find(settings.dropdownToggle);
-
-                if (dropdownToggle.length > 0 && dropdownToggle.attr('aria-expanded') === 'true') {
-                    dropdownToggle
-                        .removeClass('show')
-                        .attr('aria-expanded', 'false')
-                        .next().removeClass('show');
-                }
-            }
-        });
-
-        $(document).on('click', function (event) {
-            if (container.hasClass(stateClass) && !container.is(event.target) && container.has(event.target).length === 0) {
-                container.removeClass(stateClass);
-            }
+        $(window).scroll(function() {
+            $button.toggle($(this).scrollTop() > $(window).height());
         });
     };
 
@@ -139,237 +115,4 @@ var dropdownManager = (function ($) {
         initialize: initialize
     };
 })(jQuery);
-
-
-var tooltipVisibilityManager = (function ($) {
-    var defaults = {
-        container: '.js-tooltip',
-        opener: '.js-tooltip-toggle',
-        closer: '.js-tooltip-close',
-        stateClass: 'is-open'
-    };
-
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {}),
-            container = $(settings.container),
-            stateClass = settings.stateClass;
-
-        $(settings.opener).on('click', function (e) {
-            e.stopPropagation();
-            $(e.currentTarget).closest(settings.container).toggleClass(stateClass);
-        });
-
-        $(settings.closer).on('click', function (e) {
-            e.stopPropagation();
-
-            var tooltip = $(this).closest(settings.container);
-
-            if (tooltip.hasClass(stateClass)) {
-                tooltip.removeClass(stateClass);
-            }
-        });
-
-        $(document).on('click', function (e) {
-            var target = $(e.target);
-
-            if (container.hasClass(stateClass) && !container.is(target) && container.has(target).length === 0) {
-                container.removeClass(stateClass);
-            }
-        });
-    };
-
-    return {
-        initialize: initialize
-    };
-})(jQuery);
-
-
-var toggleFixedOnScroll = (function ($) {
-    var defaults = {
-        fixedElement: '.js-fix-element',
-        fixedTopClass: 'is-fixed-top',
-        fixedBottomClass: 'is-fixed-bottom',
-        container: '.js-fix-container',
-        header: 'header',
-        offsetTop: 0,
-        offsetBottomContainer: 0
-    };
-
-    var initialize = function(params) {
-        var settings = $.extend({}, defaults, params || {}),
-            $fixedElement = $(settings.fixedElement),
-            fixedElementHeight = $fixedElement.outerHeight(),
-            $container = $(settings.container),
-            prevScrollTop = 0;
-
-        $(window).on('scroll', function() {
-            var scrollTop = $(window).scrollTop();
-            var containerTop = $container.offset().top;
-            var containerBottom = containerTop + $container.outerHeight() - settings.offsetBottomContainer;
-            var positionTop = scrollTop < prevScrollTop ? settings.offsetTop : settings.offsetTop;
-
-            if (scrollTop <= containerTop) {
-                $fixedElement
-                    .removeClass(settings.fixedTopClass)
-                    .css({top: ''});
-            } else {
-                var isFixedTop = scrollTop + fixedElementHeight + settings.offsetBottomContainer < containerBottom;
-
-                $fixedElement
-                    .removeClass(isFixedTop ? settings.fixedBottomClass : settings.fixedTopClass)
-                    .addClass(isFixedTop ? settings.fixedTopClass : settings.fixedBottomClass)
-                    .css({top: isFixedTop ? positionTop : containerBottom - fixedElementHeight});
-            }
-
-            prevScrollTop = scrollTop;
-        }).trigger('scroll');
-    };
-
-    return {
-        initialize: initialize
-    };
-})(jQuery);
-
-
-var scrollSpy = (function ($) {
-    var defaults = {
-        navContainer: '.js-nav-container',
-        activeClass: 'active',
-        scrollDuration: 1600,
-        offset: 10
-    };
-
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {}),
-            $navContainer = $(settings.navContainer),
-            $navItems = $navContainer.find('a'),
-            sections = $navItems.map(function() {return $(this).attr('href').replace('#', '');}).get();
-
-        $(window).on('scroll', function () {
-            var scrollPosition = $(window).scrollTop();
-
-            sections.forEach(function (id) {
-                var $section = $('#' + id);
-                if ($section.length) {
-                    var offset = $section.offset().top - settings.offset,
-                        height = $section.outerHeight(true);
-
-                    if (scrollPosition >= offset && scrollPosition < offset + height) {
-                        $navItems.removeClass(settings.activeClass);
-                        $navContainer.find('[href="#' + id + '"]').addClass(settings.activeClass);
-                    }
-                }
-            });
-        });
-
-        $(window).trigger('scroll');
-    };
-
-    return {
-        initialize: initialize
-    };
-
-})(jQuery);
-
-var updateCounter = (function () {
-    var defaults = {
-        container: '.js-counter',
-        min: 0,
-        max: 1000000,
-        interval: 3000,
-        step: 2
-    };
-
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {});
-        var container = $(settings.container);
-        var counter = container.children().first();
-        var currentValue = parseFloat(counter.text()) || settings.min;
-
-        setInterval(function () {
-            var change = (Math.random() < 0.9 ? 1 : -1) * Math.random() * (Math.random() < 0.9 ? settings.step : 2);
-
-            currentValue += change;
-            currentValue = Math.max(settings.min, Math.min(settings.max, currentValue));
-            counter.text(currentValue.toFixed(0));
-        }, settings.interval);
-    };
-
-    return {
-        initialize: initialize
-    };
-})();
-
-
-var lazyLoader = (function () {
-    var defaults = {
-        rootMargin: '300px 0px',
-    };
-
-    var initialize = function (params) {
-        var settings = $.extend({}, defaults, params || {});
-
-        if ('loading' in HTMLImageElement.prototype) {
-            document.querySelectorAll('img[loading="lazy"]').forEach(img => {
-                img.src = img.dataset.src;
-            });
-        } else {
-            var script = document.createElement('script');
-            script.src = '/static/js/separate-js/lozad.min.js';
-            script.onload = () => lozad('.lozad', {rootMargin: settings.rootMargin}).observe();
-
-            document.body.appendChild(script);
-        }
-    };
-
-    return {
-        initialize: initialize
-    };
-})();
-
-
-var countDownTimer = (function () {
-    var defaults = {
-        timer: '.js-countdown',
-        deadlineHours: 1,
-        updateInterval: 1000
-    };
-
-    var timer = (minutes, seconds) => `<span>${minutes}</span><span>:</span><span>${seconds}</span>`;
-
-    var initialize = function (params) {
-        var now = new Date(),
-            settings = $.extend({}, defaults, params || {}),
-            element = $(settings.timer),
-            deadlineTime = new Date(now.getTime() + (settings.deadlineHours * 60 * 60000));
-
-        var intervalId = setInterval(function () {
-            let now = new Date().getTime();
-            let time = deadlineTime - now;
-
-            if (time <= 0) {
-                clearInterval(intervalId);
-
-                element.html(timer('00', '00'));
-
-                return;
-            }
-
-            let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-            if (minutes.toString().length < 2) minutes = '0' + minutes;
-
-            let seconds = Math.floor((time % (1000 * 60)) / 1000);
-            if (seconds.toString().length < 2) seconds = '0' + seconds;
-
-            element.html(timer(minutes, seconds));
-
-        }, settings.updateInterval);
-    };
-
-    return {
-        initialize: initialize
-    };
-})();
-
-
 
