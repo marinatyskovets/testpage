8ace97af6a9aeebb1cbdeeb63ef04ae78ac4ee73
Marina Tiskovets <m.tyskovets@gmail.com>
2025-02-24 12:26:43 +0200

    njw

---
+++ modals/static/js/separate-js/vacancies.js
@@ -0,0 +1,65 @@
+const vacancies = [
+  {
+    isHighlighted: true,
+    isAd: false,
+    title: {text: 'Frontend Developer 1',link: 'https://uk.jobsora.com/jobs'},
+    employer: {title: "Intel", link: "https://us.jobsora.com/jobs/q-frontend-developer?companyIds%5B0%5D=41252"},
+    location: {title: 'Louisiana', link: 'https://us.jobsora.com/jobs/louisiana/q-frontend-developer'},
+    salary: '$40k - $60k',
+    action: 'View',
+    timestamp: '2025-02-11T15:47:00Z',
+    tags: [
+      {title: 'JavaScript', link: 'https://uk.jobsora.com/jobs-javascript'},
+      {title: 'Frontend Developer', link: 'https://uk.jobsora.com/jobs-frontend-developer'},
+    ],
+    label: {theme: 'primary', text: 'Best Choice'}
+  },
+  {
+    isHighlighted: false,
+    isAd: false,
+    title: {text: 'Frontend Developer 2', link: 'https://uk.jobsora.com/jobs'},
+    employer: {title: 'Apple'},
+    location: {title: 'Manchester'},
+    salary: '$40k - $60k',
+    action: 'View',
+    timestamp: '2024-12-11T18:08:00Z',
+    tags: [
+      {title: 'JavaScript', link: 'https://uk.jobsora.com/jobs-javascript'},
+      {title: 'Frontend Developer', link: 'https://uk.jobsora.com/jobs-frontend-developer'},
+    ],
+    label: {theme: 'info', text: 'New'}
+  },
+  {
+    isHighlighted: false,
+    isAd: false,
+    title: {text: 'Frontend Developer 3', link: 'https://uk.jobsora.com/jobs'},
+    employer: {title: 'OpenAI', link: 'https://openai.com/', logo: '/static/img/content/openAI-logo.jpg'},
+    location: {title: 'Manchester'},
+    salary: '$40k - $60k',
+    action: 'View',
+    timestamp: '2025-02-12T17:56:00Z',
+    label: {theme: 'danger', text: 'Hot'}
+  },
+  {
+    isHighlighted: false,
+    isAd: false,
+    title: {text: 'Frontend Developer 4'},
+    employer: {title: 'KMS Technology'},
+    location: {title: 'Manchester'},
+    salary: '$40k - $60k',
+    action: 'View',
+    timestamp: '2025-02-13T11:23:00Z',
+    label: {theme: 'success', text: 'be the first'},
+    description: 'You will be responsible for writing good code and advancing the user and developer experience across their web and mobile applications. You will be responsible for writing good code and advancing the user and developer experience across their web and mobile applications.'
+  },
+  {
+    isHighlighted: false,
+    isAd: true,
+    title: {text: 'Ad'},
+    location: {title: 'Manchester'},
+    action: 'View',
+    timestamp: '2025-02-13T11:23:00Z',
+  }
+];
+
+
