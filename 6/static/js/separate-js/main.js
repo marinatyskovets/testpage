8254b0d04d727553b0535a7acaea5ab2b6e7cfd9
Marina Tiskovets <m.tyskovets@gmail.com>
2025-10-08 11:35:15 +0300

    add

---
+++ 6/static/js/separate-js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    $(document).on('show.bs.modal', '.js-modal-locker', function () {
+        locker.lock();
+    });
+
+    $(document).on('hidden.bs.modal', '.js-modal-locker', function () {
+        locker.unlock();
+    });
+
+    drawer.initialize();
+});
+
+var locker = (function ($) {
+    var settings = {
+        element: 'html',
+        lockedClass: 'no-scroll'
+    };
+
+    var lock = function () {
+        var padding = window.innerWidth - document.documentElement.clientWidth;
+
+        $(settings.element)
+            .addClass(settings.lockedClass)
+            .css('paddingRight', padding);
+    };
+
+    var unlock = function () {
+        $(settings.element)
+            .removeClass(settings.lockedClass)
+            .css('paddingRight', 0);
+    };
+
+    return {
+        lock: lock,
+        unlock: unlock
+    };
+})(jQuery);
+
+var drawer = (function ($) {
+    var settings = {
+        drawer: '.js-drawer',
+        toggler: '.js-drawer-toggler',
+        stateClass: 'is-open',
+    };
+
+    var initialize = function () {
+        var toggler = $(settings.toggler),
+            drawer = $(settings.drawer),
+            stateClass = settings.stateClass;
+
+        toggler.on('click', function () {
+            if (drawer.hasClass(stateClass)) {
+                drawer.removeClass(stateClass);
+
+                locker.unlock();
+            } else {
+                drawer.addClass(stateClass);
+
+                locker.lock();
+            }
+        });
+    };
+
+    return {
+        initialize: initialize,
+    };
+})(jQuery);
+
+
