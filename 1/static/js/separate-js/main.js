88872a8ee6ac4fb894ce3886f2130133cf3b6e7e
Marina Tiskovets <m.tyskovets@gmail.com>
2025-10-30 17:57:13 +0200

    add

--- 1/static/js/separate-js/main.js
+++ 1/static/js/separate-js/main.js
@@ -66,4 +66,55 @@ var drawer = (function ($) {
     };
 })(jQuery);
 
+var countDownTimer = (function () {
+    var defaults = {
+        timer: '.js-countdown',
+        hours: 0,
+        minutes: 10,
+        seconds: 0,
+        updateInterval: 1000
+    };
+
+    var timer = (hours, minutes, seconds) => {
+        let timeStr = '';
+        if (hours > 0) timeStr += `<span>${hours}</span><span>:</span>`;
+        timeStr += `<span>${minutes}</span><span>:</span><span>${seconds}</span>`;
+        return timeStr;
+    };
+
+    var initialize = function (params) {
+        var now = new Date(),
+            settings = $.extend({}, defaults, params || {}),
+            element = $(settings.timer),
+            // считаем общее количество миллисекунд
+            totalMs = ((settings.hours * 60 * 60) + (settings.minutes * 60) + settings.seconds) * 1000,
+            deadlineTime = new Date(now.getTime() + totalMs);
+
+        var intervalId = setInterval(function () {
+            let now = new Date().getTime();
+            let time = deadlineTime - now;
+
+            if (time <= 0) {
+                clearInterval(intervalId);
+                element.html(timer('00', '00', '00'));
+                return;
+            }
+
+            let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+            if (hours < 10) hours = '0' + hours;
+            if (minutes < 10) minutes = '0' + minutes;
+            if (seconds < 10) seconds = '0' + seconds;
+
+            element.html(timer(hours, minutes, seconds));
+
+        }, settings.updateInterval);
+    };
+
+    return {
+        initialize: initialize
+    };
+})();
 
